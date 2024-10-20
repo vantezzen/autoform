399a4445437e1b3e13e5ce9be9049e41755d74fe
Bennett <hello@vantezzen.io>
2024-10-20 11:19:31 +0200

    Fix UI library types

--- packages/mantine/src/types.ts
+++ packages/mantine/src/types.ts
@@ -1,7 +1,8 @@
 import { AutoFormProps as BaseAutoFormProps } from "@autoform/react";
 import { MantineProvider } from "@mantine/core";
+import { FieldValues } from "react-hook-form";
 
-export interface AutoFormProps<T>
+export interface AutoFormProps<T extends FieldValues>
   extends Omit<BaseAutoFormProps<T>, "uiComponents" | "formComponents"> {
   theme?: Parameters<typeof MantineProvider>[0]["theme"];
 }
--- packages/shadcn/src/components/ui/autoform/types.ts
+++ packages/shadcn/src/components/ui/autoform/types.ts
@@ -1,4 +1,5 @@
 import { AutoFormProps as BaseAutoFormProps } from "@autoform/react";
+import { FieldValues } from "react-hook-form";
 
-export interface AutoFormProps<T>
+export interface AutoFormProps<T extends FieldValues>
   extends Omit<BaseAutoFormProps<T>, "uiComponents" | "formComponents"> {}
