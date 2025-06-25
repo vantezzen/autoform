03b8ec48e48f1714754106f31fb2c86407fb1214
gitcode <website0wordpress@gmail.com>
2025-06-25 15:13:27 +0530

    feat: add code tab in docs

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,10 +1,13 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import type { MDXComponents } from "mdx/types";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import ModeTab from "@/components/landing/tabs/mode-tabs";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
     ...components,
+    ModeTab,
   };
 }
