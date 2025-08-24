926f9dc074d830f554ed2ae7f03c91282dd375d2
gitcode <website0wordpress@gmail.com>
2025-08-24 19:14:19 +0530

    tests: add mantine tests (native Enum), update select to use label (value of enum key)

--- apps/web/cypress/component/autoform/mantine-zod/basic.cy.tsx
+++ apps/web/cypress/component/autoform/mantine-zod/basic.cy.tsx
@@ -4,12 +4,21 @@ import { ZodProvider, fieldConfig } from "@autoform/zod";
 import { z } from "zod/v3";
 import { TestWrapper } from "./utils";
 
+enum Sports {
+  Football = "Football/Soccer",
+  Basketball = "Basketballs",
+  Baseball = "Baseballs",
+  Hockey = "Hockey (Ice)",
+  None = "I don't like sports",
+}
+
 describe("AutoForm Basic Tests (MANTINE-ZOD)", () => {
   const basicSchema = z.object({
     name: z.string().min(2, "Name must be at least 2 characters"),
     age: z.coerce.number().min(18, "Must be at least 18 years old"),
     email: z.string().email("Invalid email address"),
     website: z.string().url("Invalid URL").optional(),
+    sports: z.nativeEnum(Sports),
     birthdate: z.coerce.date(),
     isStudent: z.boolean(),
   });
@@ -31,6 +40,7 @@ describe("AutoForm Basic Tests (MANTINE-ZOD)", () => {
     cy.get('input[name="age"]').should("have.attr", "type", "number");
     cy.get('input[name="email"]').should("exist");
     cy.get('input[name="website"]').should("exist");
+    cy.get('input[name="sports"]').should("exist");
     cy.get('[data-dates-input="true"]').should("exist");
     cy.get('input[name="isStudent"]').should("have.attr", "type", "checkbox");
   });
@@ -47,9 +57,13 @@ describe("AutoForm Basic Tests (MANTINE-ZOD)", () => {
     cy.get('input[name="age"]').type("25");
     cy.get('input[name="email"]').type("john@example.com");
     cy.get('input[name="website"]').type("https://example.com");
-    cy.get('[data-dates-input="true"]').type("1990-01-01");
     cy.get('input[name="isStudent"]').check();
-
+    cy.get(".mantine-Select-input").eq(0).click();
+    cy.get('.mantine-Select-option[value="Hockey (Ice)"]')
+      .should("exist")
+      .and("be.visible")
+      .click();
+    cy.get('[data-dates-input="true"]').type("1990-01-01");
     cy.get('button[type="submit"]').click();
 
     cy.get("@onSubmit").should("have.been.calledOnce");
@@ -58,6 +72,7 @@ describe("AutoForm Basic Tests (MANTINE-ZOD)", () => {
       age: 25,
       email: "john@example.com",
       website: "https://example.com",
+      sports: "Hockey (Ice)",
       birthdate: new Date("1990-01-01"),
       isStudent: true,
     });
