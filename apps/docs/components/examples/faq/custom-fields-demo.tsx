5e5aa3a47f3b5949d3250ee56157fe9aa659711f
gitcode <website0wordpress@gmail.com>
2026-06-08 09:57:08 +0530

    fix(docs): remove BOM characters from demo components to resolve registry build errors

--- apps/docs/components/examples/faq/custom-fields-demo.tsx
+++ apps/docs/components/examples/faq/custom-fields-demo.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import * as React from "react";
 import { fieldConfig, ZodProvider } from "@acp-autoform/zod";
--- apps/docs/components/examples/faq/interactive-schema-demo-content.tsx
+++ apps/docs/components/examples/faq/interactive-schema-demo-content.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import React, { useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
