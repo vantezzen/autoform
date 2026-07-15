212cc51a677433cb006678348059dda6920703f1
Bennett <hello@vantezzen.io>
2026-07-15 11:18:02 +0200

    Add umami

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import type { ReactNode } from "react";
 
 const inter = Inter({
@@ -12,6 +13,12 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+
+        <Script
+          defer
+          src="https://a.vantezzen.io/script.js"
+          data-website-id="beda036f-115e-4ffe-8aee-4329ad499983"
+        />
       </body>
     </html>
   );
